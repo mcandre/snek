75a2647b084ffe7b32eb4e55262bae6a38ac93ed
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2023-05-02 22:11:35 -0500

    update unmake

--- install.mk
+++ install.mk
@@ -1,12 +1,14 @@
+.POSIX:
+.SILENT:
 .PHONY: all
 
 all:
-	@cargo install --force unmake@0.0.7
+	cargo install --force unmake@0.0.10
 
-	@go install github.com/mcandre/tug/cmd/tug@v0.0.4
-	@go mod tidy
+	go install github.com/mcandre/tug/cmd/tug@v0.0.4
+	go mod tidy
 
-	@npm install -g snyk@1.1140.0
+	npm install -g snyk
 
-	@python -m pip install --user --upgrade pip setuptools
-	@pip install --user -r requirements-dev.txt
+	python -m pip install --user --upgrade pip setuptools
+	pip install --user -r requirements-dev.txt
